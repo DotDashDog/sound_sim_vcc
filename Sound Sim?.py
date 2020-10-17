a41ddb8934b96ad9fb5dd01be5a6d7def588bc14
Linus Upson <linusupson@macbook-pro.lan>
2020-10-17 09:59:36 -0700

    Sound can now be played and volume can be modulated using pyaudio.

--- Sound Sim?.py
+++ Sound Sim?.py
@@ -1,26 +1,46 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import sounddevice as sd
-import time
-from scipy.io import wavfile
-import soundcard as sc
+#import sounddevice as sd
+#import time
+#from scipy.io import wavfile
+import wave
+#import soundcard as sc
 import pyaudio
 
 sound_speed = 343
+chunk_size = 1024
 
 #sd.play(y, sample_rate)
 #sd.wait()
 
-sample_rate, data = wavfile.read("blah-blah-blah.wav")
-#sd.play(data, sample_rate)
-#sd.wait()
-new_data = data
-batch_per_sec = 100
-batch_size = int(sample_rate/batch_per_sec)
-for i in range(batch_per_sec):
-    sd.play(data[batch_size*i:batch_size*i+1], sample_rate)
-sd.wait()
+#sample_rate, old_data = wavfile.read("blah-blah-blah.wav")
+inFile = wave.open("blah-blah-blah.wav", "rb")
+
+# instantiate PyAudio (1)
+p = pyaudio.PyAudio()
+# open stream (2)
+stream = p.open(format=p.get_format_from_width(inFile.getsampwidth()), channels=inFile.getnchannels(), rate=inFile.getframerate(), output=True)
+
+data = inFile.readframes(inFile.getnframes())
+data_arr = np.fromstring(data, dtype='int16')
+data_arr = (data_arr.reshape((inFile.getnframes(), 2))*3).astype("int16")
+data = data_arr.tostring()
+# play stream (3)
+
+stream.write(data)
+
+"""for i in range(0, len(data)-1, chunk_size):
+    chunk = data[i:i+chunk_size]
+    stream.write(chunk)"""
+
+# stop stream (4)
+stream.stop_stream()
+stream.close()
+
+inFile.close()
+# close PyAudio (5)
+p.terminate()
 
 def calc_dist(loc1, loc2):
     return math.sqrt((loc1[0]-loc2[0])**2 + (loc1[1]-loc2[1])**2)
@@ -33,7 +53,7 @@ class Environment:
     def add_avatar(self, avatar):
         np.append(self.avatars, avatar)
 
-    def amp_at_loc(self, loc):
+    def amp_at_loc(self, loc, listener):
         total_amp = 0
         for avatar in self.avatars:
             total_amp += avatar.sound.hear_amp(calc_dist(loc, avatar.loc))
@@ -53,7 +73,7 @@ class Sound:
 
     def hear_amp(self, distance):
         try:
-            return self.past_vals[distance/sound_speed*samplerate]/distance**2
+            return self.past_vals[distance/sound_speed*sample_rate]/distance**2
         except:
             return 0
 
@@ -64,4 +84,4 @@ class Avatar:
         self.sound = Sound(loc)
 
     def play_current_frame(self):
-        amp = env.amp_at_loc(self.loc)
+        amp = env.amp_at_loc(self.loc, self)
